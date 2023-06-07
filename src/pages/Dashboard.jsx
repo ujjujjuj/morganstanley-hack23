c56834487e405c618223d0e641801bdf30c5c0e1
ujjwaldimri@hotmail.com <ujjwaldimri@hotmail.com>
2023-06-07 14:05:24 +0530

    remove unnecessary pages from nav

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,23 +2,20 @@ import React, { useState } from 'react';
 
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
-import WelcomeBanner from '../partials/dashboard/WelcomeBanner';
-import DashboardAvatars from '../partials/dashboard/DashboardAvatars';
-import FilterButton from '../components/DropdownFilter';
 import Datepicker from '../components/Datepicker';
-import DashboardCard01 from '../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../partials/dashboard/DashboardCard02';
-import DashboardCard03 from '../partials/dashboard/DashboardCard03';
-import DashboardCard04 from '../partials/dashboard/DashboardCard04';
-import DashboardCard05 from '../partials/dashboard/DashboardCard05';
-import DashboardCard06 from '../partials/dashboard/DashboardCard06';
-import DashboardCard07 from '../partials/dashboard/DashboardCard07';
-import DashboardCard08 from '../partials/dashboard/DashboardCard08';
-import DashboardCard09 from '../partials/dashboard/DashboardCard09';
-import DashboardCard10 from '../partials/dashboard/DashboardCard10';
-import DashboardCard11 from '../partials/dashboard/DashboardCard11';
+import AnalyticsCard01 from '../partials/analytics/AnalyticsCard01';
+import AnalyticsCard02 from '../partials/analytics/AnalyticsCard02';
+import AnalyticsCard03 from '../partials/analytics/AnalyticsCard03';
+import AnalyticsCard04 from '../partials/analytics/AnalyticsCard04';
+import AnalyticsCard05 from '../partials/analytics/AnalyticsCard05';
+import AnalyticsCard06 from '../partials/analytics/AnalyticsCard06';
+import AnalyticsCard07 from '../partials/analytics/AnalyticsCard07';
+import AnalyticsCard08 from '../partials/analytics/AnalyticsCard08';
+import AnalyticsCard09 from '../partials/analytics/AnalyticsCard09';
+import AnalyticsCard10 from '../partials/analytics/AnalyticsCard10';
+import AnalyticsCard11 from '../partials/analytics/AnalyticsCard11';
 
-function Dashboard() {
+function Analytics() {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -37,58 +34,50 @@ function Dashboard() {
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
-            {/* Welcome banner */}
-            <WelcomeBanner />
-
-            {/* Dashboard actions */}
+            {/* Page header */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
-              {/* Left: Avatars */}
-              <DashboardAvatars />
-
+            
+              {/* Left: Title */}
+              <div className="mb-4 sm:mb-0">
+                <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">Analytics ✨</h1>
+              </div>
+          
               {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Filter button */}
-                <FilterButton align="right" />
+                  
                 {/* Datepicker built with flatpickr */}
                 <Datepicker align="right" />
-                {/* Add view button */}
-                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                  <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                  </svg>
-                  <span className="hidden xs:block ml-2">Add View</span>
-                </button>                
+                  
               </div>
-
-            </div>
-
+            
+            </div>            
+            
             {/* Cards */}
             <div className="grid grid-cols-12 gap-6">
 
-              {/* Line chart (Acme Plus) */}
-              <DashboardCard01 />
-              {/* Line chart (Acme Advanced) */}
-              <DashboardCard02 />
-              {/* Line chart (Acme Professional) */}
-              <DashboardCard03 />
-              {/* Bar chart (Direct vs Indirect) */}
-              <DashboardCard04 />
-              {/* Line chart (Real Time Value) */}
-              <DashboardCard05 />
-              {/* Doughnut chart (Top Countries) */}
-              <DashboardCard06 />
-              {/* Table (Top Channels) */}
-              <DashboardCard07 />
-              {/* Line chart (Sales Over Time) */}
-              <DashboardCard08 />
-              {/* Stacked bar chart (Sales VS Refunds) */}
-              <DashboardCard09 />
-              {/* Card (Recent Activity) */}
-              <DashboardCard10 />
-              {/* Card (Income/Expenses) */}
-              <DashboardCard11 />
-              
+              {/* Line chart (Analytics) */}
+              <AnalyticsCard01 />
+              {/*  Line chart (Active Users Right Now) */}
+              <AnalyticsCard02 />
+              {/* Stacked bar chart (Acquisition Channels) */}
+              <AnalyticsCard03 />
+              {/* Horizontal bar chart (Audience Overview) */}
+              <AnalyticsCard04 />
+              {/* Report card (Top Channels) */}
+              <AnalyticsCard05 />
+              {/* Report card (Top Pages) */}
+              <AnalyticsCard06 />
+              {/* Report card (Top Countries) */}
+              <AnalyticsCard07 />
+              {/* Doughnut chart (Sessions By Device) */}
+              <AnalyticsCard08 />
+              {/* Doughnut chart (Visit By Age Category) */}
+              <AnalyticsCard09 />
+              {/* Polar chart (Sessions By Gender) */}
+              <AnalyticsCard10 />
+              {/* Table (Top Products) */}
+              <AnalyticsCard11 />
+
             </div>
 
           </div>
@@ -100,4 +89,4 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+export default Analytics;
