92e6b22f10232f02fa3ac525fd067b49c802d3f6
ujjwaldimri@hotmail.com <ujjwaldimri@hotmail.com>
2023-06-09 20:57:41 +0530

    add more sidebar tabs

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import axios from "axios";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react"
+import { Calendar, momentLocalizer } from "react-big-calendar"
+import axios from "axios"
+import moment from "moment"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useNavigate } from "react-router-dom"
 
-import Sidebar from "../partials/Sidebar";
-import Header from "../partials/Header";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./MyCalender.css";
+import Sidebar from "../partials/Sidebar"
+import Header from "../partials/Header"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import "./MyCalender.css"
 
-const localizer = momentLocalizer(moment);
+const localizer = momentLocalizer(moment)
 
 function MyCalendar() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [events, setEvents] = useState([]);
-  const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [events, setEvents] = useState([])
+  const navigate = useNavigate()
 
   // Map each category to a color
   const categoryColors = {
@@ -26,7 +26,7 @@ function MyCalendar() {
     "Health and Wellbeing": "#e74c3c", // red
     "Government Assistance Programs": "#34495e", // dark blue
     "Employment and Career Development": "#16a085", // teal
-  };
+  }
 
   useEffect(() => {
     axios
@@ -39,19 +39,19 @@ function MyCalendar() {
           id: event._id, // Ensure this field exists in your API response
           // Use the category to determine the color of the event
           category: event.category,
-        }));
-        setEvents(fetchedEvents);
+        }))
+        setEvents(fetchedEvents)
       })
       .catch((error) => {
-        console.error("Error fetching events", error);
-      });
-  }, []);
+        console.error("Error fetching events", error)
+      })
+  }, [])
 
   const handleEventSelect = (event) => {
-    navigate(`/event/${event.id}`);
-  };
+    navigate(`/events/list/${event.id}`)
+  }
   const eventStyleGetter = (event, start, end, isSelected) => {
-    var backgroundColor = categoryColors[event.category];
+    var backgroundColor = categoryColors[event.category]
     var style = {
       backgroundColor: backgroundColor,
       borderRadius: "5px", // rounded corners
@@ -61,22 +61,22 @@ function MyCalendar() {
       display: "block",
       fontWeight: "bold", // bold text
       padding: "5px", // space around the text
-    };
+    }
     return {
       style: style,
-    };
-  };
+    }
+  }
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex overflow-hidden h-screen">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <div className="flex overflow-y-auto overflow-x-hidden relative flex-col flex-1">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        <main className=" p-10">
+        <main className="p-10">
           <div style={{ height: "500px" }}>
             <Calendar
               localizer={localizer}
@@ -91,7 +91,7 @@ function MyCalendar() {
         </main>
       </div>
     </div>
-  );
+  )
 }
 
-export default MyCalendar;
+export default MyCalendar
--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -346,40 +346,40 @@ function Campaigns() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex overflow-hidden h-screen">
 
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */} 
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <div className="flex overflow-y-auto overflow-x-hidden relative flex-col flex-1">
 
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <div className="px-4 py-8 mx-auto w-full sm:px-6 lg:px-8 max-w-9xl">
 
             {/* Page header */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            <div className="mb-8 sm:flex sm:justify-between sm:items-center">
 
               {/* Left: Title */}
               <div className="mb-4 sm:mb-0">
-                <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">Campaigns ✨</h1>
+                <h1 className="text-2xl font-bold md:text-3xl text-slate-800">Campaigns ✨</h1>
               </div>
 
               {/* Right: Actions */}
-              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+              <div className="grid grid-flow-col gap-2 justify-start sm:auto-cols-max sm:justify-end">
                 {/* Search form */}
                 <SearchForm />
                 {/* Filter button */}
                 <FilterButton align="right" />
                 {/* Create campaign button */}
-                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                  <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                <button className="text-white bg-indigo-500 btn hover:bg-indigo-600">
+                  <svg className="w-4 h-4 opacity-50 fill-current shrink-0" viewBox="0 0 16 16">
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
-                  <span className="hidden xs:block ml-2">Create Campaign</span>
+                  <span className="hidden ml-2 xs:block">Create Campaign</span>
                 </button>
               </div>
 
