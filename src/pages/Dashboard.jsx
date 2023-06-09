e62caaa3e1aac75d9cc461f55867a722e2f3e96e
ujjwaldimri@hotmail.com <ujjwaldimri@hotmail.com>
2023-06-09 21:04:09 +0530

    add some more charts to the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,30 +3,27 @@ import React, { useState } from "react"
 import Sidebar from "../partials/Sidebar"
 import Header from "../partials/Header"
 
-
 function Analytics() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex overflow-hidden h-screen">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <div className="flex overflow-y-auto overflow-x-hidden relative flex-col flex-1">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <div className="px-4 py-8 mx-auto w-full sm:px-6 lg:px-8 max-w-9xl">
             {/* Page header */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            <div className="mb-8 sm:flex sm:justify-between sm:items-center">
               {/* Left: Title */}
               <div className="mb-4 sm:mb-0">
-                <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
+                <h1 className="text-2xl font-bold md:text-3xl text-slate-800">
                   Analytics ✨
                 </h1>
               </div>
-
-            
             </div>
 
             {/* Cards */}
@@ -47,66 +44,75 @@ function Analytics() {
             </div> */}
             {/* Main Container div */}
             <div className="flex flex-col gap-4">
-              <div className="flex space-between gap-4 flex-col xl:flex-row">
+              <div className="flex flex-col gap-4 space-between xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64833108-84c7-4413-8f37-186c64f2ca26&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=3600&theme=light&autoRefresh=true"
+                  // src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648327f4-fdb9-4ec0-82c8-98071ea74cc5&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=3600&theme=light&autoRefresh=true"
+                  // src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=3600&theme=light&autoRefresh=true"
+                  // src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
               {/* div for 3 charts in a row */}
-              <div className="flex space-between gap-4 flex-col xl:flex-row">
+              <div className="flex flex-col gap-4 space-between xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648056d4-b1b7-41ea-8cc7-8619fa588a67&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648056d4-b1b7-41ea-8cc7-8619fa588a67&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648058a3-ba3a-4dd1-8beb-cde198a42548&maxDataAge=60&theme=light&autoRefresh=true"
+                  // src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648058a3-ba3a-4dd1-8beb-cde198a42548&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64820078-7f2b-4fbe-8ee1-3368aaeeb163&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64820078-7f2b-4fbe-8ee1-3368aaeeb163&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
               {/* div for 3 charts in a row */}
-              <div className="flex space-between gap-4 flex-col xl:flex-row">
+              <div className="flex flex-col gap-4 space-between xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805d32-a88f-4f32-8938-011d4f279f5a&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805d32-a88f-4f32-8938-011d4f279f5a&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805db0-abe9-4522-8d84-1e7088a94264&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805db0-abe9-4522-8d84-1e7088a94264&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805e14-23c6-487f-873f-74915df1e607&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805e14-23c6-487f-873f-74915df1e607&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
               {/* div for 2 charts in a row */}
-              <div className="flex space-between gap-4 flex-col lg:flex-row">
+              <div className="flex flex-col gap-4 space-between lg:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] lg:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64821d11-1450-4ec6-81d4-04d8efa8d4dd&maxDataAge=60&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64821d11-1450-4ec6-81d4-04d8efa8d4dd&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] lg:flex-1"
-                  src="https://charts.mongodb.com/charts-project-0-zwpmz/embed/charts?id=647fa2e3-1b39-4a97-8822-968c68c6ef81&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project-0-zwpmz/embed/charts?id=647fa2e3-1b39-4a97-8822-968c68c6ef81&maxDataAge=300&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
-
+              <div className="flex flex-col gap-4 space-between lg:flex-row">
+                <iframe
+                  className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648058a3-ba3a-4dd1-8beb-cde198a42548&maxDataAge=300&theme=light&autoRefresh=true"
+                ></iframe>
+                <iframe
+                  className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648327f4-fdb9-4ec0-82c8-98071ea74cc5&maxDataAge=3600&theme=light&autoRefresh=true"
+                ></iframe>
+              </div>
             </div>
           </div>
         </main>
