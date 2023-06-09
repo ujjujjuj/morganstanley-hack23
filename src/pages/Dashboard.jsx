9a395a245da822cb47532b5bb6ebb6ca0cffb050
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-06-09 18:18:15 +0530

    pages update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,10 @@ import React, { useState } from "react"
 
 import Sidebar from "../partials/Sidebar"
 import Header from "../partials/Header"
-import Datepicker from "../components/Datepicker"
 
 
 function Analytics() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
-
   return (
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
@@ -17,7 +15,6 @@ function Analytics() {
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Page header */}
@@ -29,11 +26,7 @@ function Analytics() {
                 </h1>
               </div>
 
-              {/* Right: Actions */}
-              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Datepicker built with flatpickr */}
-                <Datepicker align="right" />
-              </div>
+            
             </div>
 
             {/* Cards */}
@@ -54,10 +47,10 @@ function Analytics() {
             </div> */}
             {/* Main Container div */}
             <div className="flex flex-col gap-4">
-              <div className="flex space-between gap-4 flex-col 2xl:flex-row">
+              <div className="flex space-between gap-4 flex-col xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805c35-4539-4d14-8d50-9807fe142564&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[320px] 2xl:flex-1"
@@ -76,37 +69,37 @@ function Analytics() {
               <div className="flex space-between gap-4 flex-col xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648056d4-b1b7-41ea-8cc7-8619fa588a67&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648056d4-b1b7-41ea-8cc7-8619fa588a67&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648058a3-ba3a-4dd1-8beb-cde198a42548&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=648058a3-ba3a-4dd1-8beb-cde198a42548&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project-0-zwpmz/embed/charts?id=647fa2e3-1b39-4a97-8822-968c68c6ef81&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64820078-7f2b-4fbe-8ee1-3368aaeeb163&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
               {/* div for 3 charts in a row */}
               <div className="flex space-between gap-4 flex-col xl:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805d32-a88f-4f32-8938-011d4f279f5a&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805d32-a88f-4f32-8938-011d4f279f5a&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805db0-abe9-4522-8d84-1e7088a94264&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805db0-abe9-4522-8d84-1e7088a94264&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] xl:flex-1"
-                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805e14-23c6-487f-873f-74915df1e607&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64805e14-23c6-487f-873f-74915df1e607&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
               </div>
               {/* div for 2 charts in a row */}
               <div className="flex space-between gap-4 flex-col lg:flex-row">
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] lg:flex-1"
-                  src="https://charts.mongodb.com/charts-project-0-zwpmz/embed/charts?id=647fa2e3-1b39-4a97-8822-968c68c6ef81&maxDataAge=3600&theme=light&autoRefresh=true"
+                  src="https://charts.mongodb.com/charts-project1-tuzcu/embed/charts?id=64821d11-1450-4ec6-81d4-04d8efa8d4dd&maxDataAge=60&theme=light&autoRefresh=true"
                 ></iframe>
                 <iframe
                   className="bg-white border-none rounded-sm h-[480px] lg:flex-1"
--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -241,7 +241,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="/events/customers"
+                              to="events/AllEvents"
                               className={({ isActive }) =>
                                 "block transition duration-150 truncate " +
                                 (isActive
@@ -250,7 +250,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                User List
+                              Manage Events
                               </span>
                             </NavLink>
                           </li>
@@ -266,7 +266,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Events List
+                                Events View
                               </span>
                             </NavLink>
                           </li>
