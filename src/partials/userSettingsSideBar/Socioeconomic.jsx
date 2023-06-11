25ca0492840e4332a7bb6d741d801cc13d1b5550
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-06-11 19:09:59 +0530

    toast alerts

--- src/partials/userSettingsSideBar/Socioeconomic.jsx
+++ src/partials/userSettingsSideBar/Socioeconomic.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios" // import axios for making HTTP requests
+import { toast } from "react-toastify"
 
 function Socioeconomic(props) {
   const [cleanWaterAccess, setCleanWaterAccess] = useState("") // state variable for currentEmployment
@@ -7,7 +8,6 @@ function Socioeconomic(props) {
   const [housingType, setHousingType] = useState("") // state variable for jobTraining
   const [transportationAccess, setTransportationAccess] = useState("") // state variable for workNature
 
-  // const id = "d667476a-6f64-47c4-8eb7-4d4504927b60"; // Constant user ID for now
   const id = props.id
   useEffect(() => {
     const fetchUserDetail = async () => {
@@ -47,10 +47,10 @@ function Socioeconomic(props) {
         }
       )
       console.log(response.data)
-      alert("Profile updated successfully.")
+      toast.success("Profile updated successfully.")
     } catch (error) {
       console.error("Failed to update profile.", error)
-      alert("Failed to update profile.")
+      toast.error("Failed to update profile.")
     }
   }
   return (
